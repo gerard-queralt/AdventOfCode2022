7bc4f71a02733a5b21361eaf214e10597ab893b0
Gerard <gqueraltferre@gmail.com>
2022-12-03 21:09:09 +0100

    added more helper functions to CppFileReader

--- src/fileReader/CppFileReader.cpp
+++ src/fileReader/CppFileReader.cpp
@@ -20,3 +20,35 @@ std::string CppFileReader::ReadInputOfDay(int day)
 
 	return fileContents;
 }
+
+std::vector<std::string> CppFileReader::InputAsLines(const std::string& inputContents)
+{
+	return SplitStringAtChar(inputContents, '\n');
+}
+
+std::vector<std::string> CppFileReader::InputAsLines(int day)
+{
+	std::string fileContents = ReadInputOfDay(day);
+	return InputAsLines(fileContents);
+}
+
+std::vector<std::string> CppFileReader::SplitStringAtChar(const std::string& string, char splitAt)
+{
+	std::vector<std::string> splitString {""};
+	for (char c : string)
+	{
+		if (c != splitAt)
+		{
+			int lastIndex = splitString.size() - 1;
+			splitString[lastIndex].push_back(c);
+		}
+		else
+		{
+			//this will cause an extra line to be added
+			splitString.push_back("");
+		}
+	}
+	//remove the extra line
+	splitString.pop_back();
+	return splitString;
+}
--- src/fileReader/CppFileReader.h
+++ src/fileReader/CppFileReader.h
@@ -1,7 +1,11 @@
 #include <string>
+#include <vector>
 
 class CppFileReader
 {
 public:
     static std::string ReadInputOfDay(int day);
+    static std::vector<std::string> InputAsLines(const std::string& inputContents);
+    static std::vector<std::string> InputAsLines(int day);
+    static std::vector<std::string> SplitStringAtChar(const std::string& string, char splitAt);
 };
