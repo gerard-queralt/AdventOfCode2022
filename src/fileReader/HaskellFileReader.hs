cb15d24937b4428befcbc859afabd1c38bbf18bb
Gerard <gqueraltferre@gmail.com>
2022-12-04 12:38:24 +0100

    added helper function to HaskellFileReader to split strings

--- src/fileReader/HaskellFileReader.hs
+++ src/fileReader/HaskellFileReader.hs
@@ -4,3 +4,9 @@ module HaskellFileReader where
     let dayInputPath = "..\\day" ++ (show day) ++ "\\input.txt"
     inputFile <- readFile dayInputPath
     return inputFile
+
+  splitStringAt :: String -> Char -> [String]
+  splitStringAt s splitAt =
+    case break (==splitAt) s of
+      (s1, splitAt:s2) -> s1 : splitStringAt s2 splitAt
+      (s1, "") -> [s1]
