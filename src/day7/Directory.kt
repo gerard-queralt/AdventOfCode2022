7df7df240549c0286359ab0f69694c6990157731
Gerard <gqueraltferre@gmail.com>
2022-12-09 20:11:39 +0100

    day 7 part 1

--- src/day7/Directory.kt
+++ src/day7/Directory.kt
@@ -5,50 +5,47 @@ import java.io.File
 class Directory(val parent: Directory? = null) : SystemItem {
     override val size: Long
         get() {
-            return contents.sumOf(SystemItem::size)
+            return contents.values.sumOf(SystemItem::size)
         }
-    private val contents: MutableList<SystemItem> = ArrayList()
+    private val contents: MutableMap<String, SystemItem> = HashMap()
 
-    fun addItem(item: SystemItem) {
-        contents.add(item)
+    fun addItem(itemName: String, item: SystemItem) {
+        contents.putIfAbsent(itemName, item)
     }
 
     companion object{
         fun createDirectoriesFromInput(input: File) : Collection<Directory> {
-            val dirDictionary: MutableMap<String, Directory> = HashMap()
+            val dirs: MutableSet<Directory> = HashSet()
             var currentDir: Directory? = null
             input.forEachLine {line ->
                 if (line[0] == '$') { //command
                     val command = line.substring(2, 4)
                     if (command == "cd") {
                         val dirName = line.substring(5)
-                        if (dirName == "..") {
-                            currentDir = currentDir!!.parent
+                        currentDir = if (dirName == "..") {
+                            currentDir!!.parent
+                        } else {
+                            //if there is a current dir, move to child
+                            //else create new root
+                            (currentDir?.contents?.get(dirName) ?: Directory(null)) as Directory?
                         }
-                        else {
-                            if (!dirDictionary.containsKey(dirName))
-                                dirDictionary[dirName] = Directory(null)
-                            currentDir = dirDictionary[dirName]
-                        }
+                        dirs.add(currentDir!!)
                     }
                 }
                 else { //ls result
                     if (line.substring(0, 3) == "dir"){
                         val dirName = line.substring(4)
-                        if (!dirDictionary.containsKey(dirName)) {
-                            val newDir = Directory(currentDir)
-                            dirDictionary[dirName] = newDir
-                            currentDir!!.addItem(newDir)
-                        }
+                        currentDir!!.addItem(dirName, Directory(currentDir))
                     }
                     else { //file
                         //note: this assumes no directory will be listed twice
                         val fileSize = line.substringBefore(" ").toLong()
-                        currentDir!!.addItem(File(fileSize))
+                        val fileName = line.substringAfter(" ")
+                        currentDir!!.addItem(fileName, File(fileSize))
                     }
                 }
             }
-            return dirDictionary.values
+            return dirs
         }
     }
 }
--- src/day7/Filesystem.kt
+++ src/day7/Filesystem.kt
@@ -3,8 +3,6 @@ package day7
 import fileReader.KotlinFileReader
 
 fun main() {
-    //TODO: does not work, incorrectly assumed directory names are unique
-    //at least that's where I think the issue is
     val input = KotlinFileReader.readInputOfDay(7)
     val dirs: Collection<Directory> = Directory.createDirectoriesFromInput(input)
     val part1: Long = dirs.map(Directory::size).filter { it <= 100000 }.sum()
