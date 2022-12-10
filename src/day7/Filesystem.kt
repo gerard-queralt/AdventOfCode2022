b3eef699d83f4a18edcf4c2fa79db579fbd20d76
Gerard <gqueraltferre@gmail.com>
2022-12-10 02:03:42 +0100

    day 7 part 2

--- src/day7/Filesystem.kt
+++ src/day7/Filesystem.kt
@@ -5,6 +5,12 @@ import fileReader.KotlinFileReader
 fun main() {
     val input = KotlinFileReader.readInputOfDay(7)
     val dirs: Collection<Directory> = Directory.createDirectoriesFromInput(input)
-    val part1: Long = dirs.map(Directory::size).filter { it <= 100000 }.sum()
+    val dirSizes: List<Long> = dirs.map(Directory::size)
+    val part1: Long = dirSizes.filter { it <= 100000 }.sum()
     println("Part 1: $part1")
+    val totalMem: Long = 70000000
+    val unusedMem = totalMem - dirSizes.max()
+    val neededMem: Long = 30000000
+    val sizeOfDirToDelete = dirSizes.sorted().find { neededMem <= unusedMem + it }
+    println("Part 2: $sizeOfDirToDelete")
 }
